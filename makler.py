1e648bf1828a37443cbc84e12d55a1f142f13c3e
EWORRING <emmaworrings@web.de>
2024-02-19 10:45:46 +0100

    added code

--- makler.py
+++ makler.py
@@ -1,1 +1,66 @@
-goodbye my lover, goodbye my friend!
+
+import json
+print("Befinden Sie sich im ersten Raum?")
+nextRoom = input()
+i = 0
+wohnung = []
+
+def raumMessen(raumnummer):
+    print("Wie soll der Raum heißen")
+    raumName = input()
+    print("Hat der Raum 4 Ecken?")
+    if(input()=="Nein"):
+        print("Teilen Sie den Raum in 2 Vierecke auf!!")
+        print("Geben Sie die Maße vom ersten Raum ein. Wielang ist die erste Wand?")
+        wandeinsraumeins = float(input())
+        print("Geben Sie die Maße vom ersten Raum ein. Wielang ist die zweite Wand?")
+        wandzweiraumeins = float(input())
+        print("Geben Sie die Maße vom zweiten Raum ein. Wielang ist die erste Wand?")
+        wandeinsraumzwei = float(input())
+        print("Geben Sie die Maße vom zweiten Raum ein. Wielang ist die zweiten Wand?")
+        wandzweiraumzwei = float(input())
+        erstesMaß = wandeinsraumeins*wandzweiraumeins
+        zweitesMaß= wandeinsraumzwei*wandzweiraumzwei
+        gesamtFlaeche = erstesMaß+zweitesMaß
+    else:
+        print("Wielang ist die erste Wand?")
+        erstesMaß = float(input())
+        print("Wielang ist die zweite Wand?")
+        zweitesMaß = float(input())
+        gesamtFlaeche = erstesMaß*zweitesMaß
+    raumnummer = {
+        "raumName" : raumName,
+        "erstesMaß": erstesMaß,
+        "zweitesMaß":zweitesMaß,
+        "gesamtFlaeche": gesamtFlaeche
+    }
+    print(raumnummer)
+    wohnung.append(raumnummer)
+    print(wohnung)
+
+    
+
+while(nextRoom == "Ja"):
+    i = i+1
+    raumMessen(i)
+    print("gibt es noch einen Raum?")
+    nextRoom = input()
+
+if(nextRoom == "Nein"):
+    wohnungGesamt = 0
+    for dict in wohnung:
+        wohnungGesamt = wohnungGesamt+dict["gesamtFlaeche"]
+    round(wohnungGesamt,2)
+    print("Die Gesamtfläche beträgt:")
+    print(round(wohnungGesamt,2))
+    print("Die durchschnittliche Fläche pro Raum beträgt:")
+    print(round(wohnungGesamt/i,2))
+
+    
+
+    
+
+    
+
+
+
